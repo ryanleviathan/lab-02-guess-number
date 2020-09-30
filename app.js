eec2688eb8fca94e61c6b926b615123f67f05cdb
ryanleviathan <ryan.m.carreras@gmail.com>
2020-09-30 00:10:13 -0700

    MVP done, buggy guess countdown output

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
-// import functions and grab DOM elements
+const generateNumber = Math.floor(Math.random() * 20);
+const numberChecker = document.getElementById('numberChecker');
+const userInput = document.getElementById('input');
+const output = document.getElementById('output');
+const reset = document.getElementById('reset');
 
-// initialize state
+console.log(generateNumber);
 
-// set event listeners to update state and DOM
+let guessesLeft = 4;
+
+numberChecker.addEventListener('click', () => {
+    const tooLow = (userInput.valueAsNumber < generateNumber);
+    const tooHigh = (userInput.valueAsNumber > generateNumber);
+    const correct = (generateNumber);
+    const guessesCounter = document.getElementById('guessesCounter');
+    guessesLeft--;
+
+    if (correct) {
+        output.textContent = 'You win... this time.';
+    }
+
+    if (tooLow) {
+        output.textContent = 'You have to guess a number higher than that.';
+    }
+
+    if (tooHigh) {
+        output.textContent = 'You have to guess a number lower than that.';
+    }
+
+    if (guessesLeft === 3) {
+        guessesCounter.textContent = 'You only have three guesses left.';
+    }
+    
+    if (guessesLeft === 2) {
+        guessesCounter.textContent = 'You only have two guesses left. You are running out of guesses.';
+    }
+    
+    if (guessesLeft === 1) {
+        guessesCounter.textContent = 'You only have one guess left. Trust your instincts.';
+    }
+
+    if (guessesLeft === 0) {
+        output.textContent = 'You lose... Too bad.';
+    }
+
+});
+
+reset.addEventListener('click', () => {
+    location.reload();
+});
